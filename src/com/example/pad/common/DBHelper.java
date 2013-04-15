5ecca4509a6ba05fab30cea150fe7acf9381dcd0
cming.xu <cming.xu@gmail.com>
2013-04-15 21:40:25 +0800

    model added

---
+++ src/com/example/pad/common/DBHelper.java
@@ -0,0 +1,11 @@
+package com.example.pad.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xcm
+ * Date: 4/15/13
+ * Time: 8:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DBHelper {
+}
---
+++ src/com/example/pad/models/Base.java
@@ -0,0 +1,11 @@
+package com.example.pad.models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xcm
+ * Date: 4/15/13
+ * Time: 8:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Base {
+}
